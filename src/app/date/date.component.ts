f4a3480c30eeaf5436db70587c92ee15ccd635e1
Gago <egagik69@gmail.com>
2022-11-28 01:33:27 +0400

    clear changDate

--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -22,45 +22,39 @@ export class DateComponent implements OnInit {
     }
 
     changeDate() {
+        this.hour = Math.floor(+this.second / 3600);
+        let minFloor = (+this.second / 3600 - this.hour) * 60;
+        this.minute = Math.floor(minFloor);
+        this.time = Math.floor((minFloor - this.minute) * 60);
 
-
-        this.hour = +this.second / 3600;
-        this.hour = Math.floor(this.hour);
-
-        this.minute = +this.second - this.hour * 3600;
-
-        this.minute = +this.second / 60;
-        this.minute = Math.floor(this.minute);
-        this.time = +this.second - this.minute * 60;
-
-        if (this.hour === 1) {
-            this.minute = +this.minute - 60;
-        }
-        if (this.minute < 10) {
-            this.minute = `0${this.minute}`
+        if (this.time === 0) {
+            this.time = 59;
+            this.minute = +this.minute - 1;
         }
 
-        if (this.time < 10) {
-            this.time = `0${this.time}`
-        }
-
+    
 
     }
 
+
     timer() {
-
+      
         let interval = setInterval(() => {
-
             this.time = +this.time - 1;
-            if (this.hour === 1 && this.time === 0) {
-                this.hour = 0;
+            if (this.time === 0 && this.minute === 0 && this.hour > 0) {
                 this.minute = 59;
+                this.time = 59;
+                this.hour = +this.hour - 1;
+            }
+       
+            if (this.time === 0) {
+                this.time = 59;
+                this.minute = +this.minute - 1;
+                
             }
 
-            if (this.time === 0) {
-                this.minute = +this.minute - 1;
-                this.time = 59;
-            }
+         
+
             if (this.minute < 0) {
                 clearInterval(interval);
                 this.minus.emit(this.minute);
